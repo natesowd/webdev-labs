7ab637a40d909728fb44c9f7d47e0def51983f4f
natesowd <natesowder2023@u.northwestern.edu>
2022-05-27 19:56:53 -0500

    Lab03 Final

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,58 +10,87 @@ FROM users;
 
 
 -- Exercise 3
-
+SELECT id, first_name, last_name 
+FROM users
+ORDER BY last_name;
 
 
 
 -- Exercise 4
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26;
 
 
 
 -- Exercise 5
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26 OR user_id=12;
 
 
 
 -- Exercise 6
-
+SELECT count(*)
+FROM posts;
 
 
 
 -- Exercise 7
-
+SELECT user_id, count(*) 
+FROM comments 
+GROUP BY user_id 
+ORDER BY count(*) desc;
 
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE posts.user_id=26 OR posts.user_id=12;
 
 
 
 -- Exercise 9
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+INNER JOIN following ON posts.user_id = following.following_id
+WHERE following.user_id=26;
 
+-- SELECT id, user_id
+-- FROM posts;
 
-
+-- SELECT *
+-- FROM following;
 
 -- Exercise 10
 
-
-
+SELECT posts.id, posts.pub_date, following.following_id, users.username
+FROM posts
+INNER JOIN following ON posts.user_id = following.following_id
+INNER JOIN users ON users.id = posts.user_id
+WHERE following.user_id=26
+ORDER BY posts.pub_date desc;
 
 -- Exercise 11
-
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 219, now());
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 220, now());
+INSERT INTO bookmarks (user_id, post_id, timestamp)
+VALUES(26, 221, now());
 
 
 
 -- Exercise 12
-
-
-
-
+DELETE FROM bookmarks WHERE user_id = 26 and post_id = 219;
+DELETE FROM bookmarks WHERE user_id = 26 and post_id = 220;
+DELETE FROM bookmarks WHERE user_id = 26 and post_id = 221;
 -- Exercise 13
 
 
+UPDATE users SET email = 'knick2022@gmail.com' where id = 26;
 
 
 -- Exercise 14
